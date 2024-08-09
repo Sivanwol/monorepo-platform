ba892b87e01ec168b6babe554aed0011e88a14cd
Sivan wolberg <sivan@wolberg.pro>
2024-08-09 16:35:30 +0300

    chore: small changes

--- packages/backoffice-api/src/router/settings.ts
+++ packages/backoffice-api/src/router/settings.ts
@@ -1,12 +1,18 @@
 import { EdgeConfig } from "@app/utils";
 import { get } from "@vercel/edge-config";
 import { protectedProcedure } from "../trpc";
+import { env } from "@app/auth/env";
 
 export const settingsRouter = {
   checkMaintenanceStatus: protectedProcedure.query(async () => {
     console.log("checking maintenance status");
-    debugger; // Uncomment to debug the edge function
-    const res = await get(EdgeConfig.backofficeMaintenance)
-    return { status: res === "true" };
+    try {
+      const res = await get(EdgeConfig.backofficeMaintenance);
+      console.log("maintenance status:", res);
+      return { status: !!res };
+    } catch (error) {
+      console.error("Error checking maintenance status:", error);
+      return { status: false };
+    }
   }),
-}
+};
