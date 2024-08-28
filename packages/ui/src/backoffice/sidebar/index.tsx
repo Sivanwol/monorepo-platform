7b9e1e1535efad291f8d709c3432432d6f32f983
Sivan wolberg <sivan@wolberg.pro>
2024-08-28 19:20:17 +0300

    fix: formatting

--- packages/ui/src/backoffice/sidebar/index.tsx
+++ packages/ui/src/backoffice/sidebar/index.tsx
@@ -74,10 +74,11 @@ export const SidebarArea = ({
 }: SidebarProps) => {
   return (
     <aside
-      className={`z-9999 w-72.5 border-stroke dark:border-stroke-dark dark:bg-gray-dark absolute left-0 top-0 flex h-screen flex-col overflow-y-auto border-r bg-white lg:static lg:translate-x-0 ${sidebarOpen
-        ? "translate-x-0 duration-300 ease-linear"
-        : "-translate-x-full"
-        }`}
+      className={`z-9999 w-72.5 border-stroke dark:border-stroke-dark dark:bg-gray-dark absolute left-0 top-0 flex h-screen flex-col overflow-y-auto border-r bg-white lg:static lg:translate-x-0 ${
+        sidebarOpen
+          ? "translate-x-0 duration-300 ease-linear"
+          : "-translate-x-full"
+      }`}
     >
       {/* <!-- SIDEBAR HEADER --> */}
       <div className="py-5.5 lg:py-6.5 flex items-center justify-between gap-2 px-6 xl:py-10">
