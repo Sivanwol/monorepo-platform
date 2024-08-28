d6350f1672ab7474304d22f1e47bb77b95c03910
Sivan wolberg <sivan@wolberg.pro>
2024-08-28 16:04:34 +0300

    ci: try make the ci to pass

--- tooling/eslint/base.js
+++ tooling/eslint/base.js
@@ -58,6 +58,7 @@ export default tseslint.config(
       ...turboPlugin.configs.recommended.rules,
       "@typescript-eslint/no-unsafe-assignment": "warn",
       "@typescript-eslint/no-unsafe-call": "warn",
+      "@typescript-eslint/no-unsafe-return": "warn",
       "@typescript-eslint/no-unused-vars": [
         "warn",
         { argsIgnorePattern: "^_", varsIgnorePattern: "^_" },
