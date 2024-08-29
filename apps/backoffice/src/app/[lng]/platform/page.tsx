231a1580777d4a07b7f8c557c59b8ec9f5d13870
Sivan wolberg <sivan@wolberg.pro>
2024-08-30 01:19:10 +0300

    chore: Remove unused runtime variable in page.tsx

--- apps/backoffice/src/app/[lng]/platform/page.tsx
+++ apps/backoffice/src/app/[lng]/platform/page.tsx
@@ -1,6 +1,5 @@
 import { redirect } from "next/navigation";
 
-export const runtime = "edge";
 
 export default function PlatformPage() {
   // You can await this here if you don't want to show Suspense fallback below
