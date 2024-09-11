c066cc0821efad27634f1b6ec61bdc3f3ce850b2
Sivan wolberg <sivan@wolberg.pro>
2024-09-11 18:37:50 +0300

    Refactor dockerfile.backoffice: Update start command to use "CMD" instead of "RUN" and add PM2 configuration

---
+++ apps/backoffice/ecosystem.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  apps: [
+    {
+      name: "backoffice",
+      script: "npm",
+      args: "run start:docker",
+      env: {
+        PORT: 3001
+      },
+      // Additional settings
+      instances: 1, // Number of instances to run
+      autorestart: true, // Automatically restart the app if it crashes
+      watch: false, // Disable watching file changes
+      max_restarts: 5, // Limit the number of restart attempts
+      restart_delay: 1000, // Delay between restart attempts in milliseconds
+      max_memory_restart: "300M" // Restart the app if it exceeds 300MB memory usage
+    }
+  ]
+};
