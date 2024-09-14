0611ea6afdff953ef9c0cf59a9fba0bbd69dbe49
Sivan wolberg <sivan@wolberg.pro>
2024-09-14 11:39:41 +0300

    Refactor DrizzleLogger.ts: Remove unused import and commented code

--- packages/db/src/DrizzleLogger.ts
+++ packages/db/src/DrizzleLogger.ts
@@ -1,10 +1,8 @@
-import { DefaultLogger, Logger } from "drizzle-orm/logger";
-
+import type { Logger } from "drizzle-orm/logger";
 import { logger } from "@app/utils";
 
 export class DrizzleLogger implements Logger {
   logQuery(query: string, params: unknown[]): void {
     logger.info(`Executing query: ${query}`, { params });
-    // Write to file, stdout, etc.
   }
 }
